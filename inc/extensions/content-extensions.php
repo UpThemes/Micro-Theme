f896220926fe0577a7af27a0051262e49e39f09b
Chip Bennett <chip@chipbennett.net>
2011-09-17 17:41:30 -0500

    Output home_url() in post icon link for standard post format type

--- inc/extensions/content-extensions.php
+++ inc/extensions/content-extensions.php
@@ -461,11 +461,12 @@ function micro_post_title(){
 	
 	/* Post Icons */
 	$post_icon_pref = ( isset( $up_options->post_format_icon ) ? $up_options->post_format_icon : 'left' );
-	$post_icon_pref = 'icon-'.$post_icon_pref;
+	$post_icon_pref = 'icon-' . $post_icon_pref;
+	$post_icon_link = ( get_post_format() ? get_post_format_link( get_post_format() ) : home_url( '/' ) );
 		
 		if( ! $postformat || $postformat == 'aside' || $postformat == 'status' || ( isset( $up_options->showtitle ) && in_array( $postformat, $up_options->showtitle ) ) ) : ?>
 		<div class="post-type <?php echo $post_icon_pref;?>">
-			<a href="<?php echo get_post_format_link( get_post_format() ); ?>"><?php echo $post->ID; ?></a>
+			<a href="<?php echo $post_icon_link; ?>"><?php echo $post->ID; ?></a>
 		</div>
 	
 		<?php
@@ -481,7 +482,7 @@ function micro_post_title(){
 	$title = get_post_meta( get_the_ID(), 'link-text', true );
 	$title = $title ? $title . " &#8594;" : get_the_title();
 	
-	if( !$postformat || $postformat == 'aside' || $postformat == 'status' || ( isset( $up_options->showtitle ) && in_array( $postformat, $up_options->showtitle ) ) ):
+	if( ! $postformat || $postformat == 'aside' || $postformat == 'status' || ( isset( $up_options->showtitle ) && in_array( $postformat, $up_options->showtitle ) ) ):
 	    if( ( $url && get_post_format( 'link' ) ) || ! is_singular() ): ?>
 			<h2 class="title"><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
 	    <?php else: ?>
