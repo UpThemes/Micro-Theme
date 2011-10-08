271cd4f8697ff8066380a250adbbfbaea935de16
Chip Bennett <chip@chipbennett.net>
2011-10-08 17:54:23 -0500

    Re: Issue #1 add phpDoc to theme-options files

--- theme-options/layout-and-display_1.php
+++ theme-options/layout-and-display_1.php
@@ -1,16 +1,38 @@
 <?php
-/*  Array Options:
-   
-   name (string)
-   desc (string)
-   id (string)
-   type (string) - text, color, image, select, multiple, textarea, page, pages, category, categories
-   value (string) - default value - replaced when custom value is entered - (text, color, select, textarea, page, category)
-   options (array)
-   attr (array) - any form field attributes
-   url (string) - for image type only - defines the default image
-    
-*/
+/**
+ * Theme Layout and Display Settings Functions file
+ * 
+ * The /theme-options/layout-and-display_1.php file defines
+ * the layout and display options for the Theme.
+ * 
+ * How to use this file:
+ * 1) Save this template to the 'theme-options' folder in the Theme root
+ * 2) Change the file name to this syntax (remember to add the .php 
+ *    extension): tab-name_#.php, where:
+ *    - tab-name = Name of the Tab as it will appear in the Admin menu
+ *    - # = position tab will appear, relative to other tabs
+ * 3) Add options to the $options array
+ * 4) BOOM!
+ * 
+ * To add additional options, add arrays to the $options
+ * array, with each new array containing the following
+ * array keys:
+ * - key 	name	string	(required)	option name
+ * - key	desc	string	(required)	option description
+ * - key	id		string	(required)	option slug
+ * - key	type	string	(required)	option type; one of: text, color, image, select, multiple, textarea, page, pages, category, categories
+ * - key	value	string	(required)	default option value, replaced when custom value is entered (text, color, select, textarea, page, category)
+ * - key	options	array	(optional)	associative array of valid options for select-type options, in the form of "Name" => "slug"
+ * - key	attr	array	(optional)	form-field attributes
+ * - keys	url		string	(optional)	default-image URL, for image-type options
+ * 
+ * @package 	Micro
+ * @copyright	Copyright (c) 2011, UpThemes
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
+ *
+ * @since 		Micro 1.0
+ */
+
 $options = array (
 
     array(  "name" => "Theme Layout",
